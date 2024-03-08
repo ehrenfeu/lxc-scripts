2e2984efc417a01d83fb9ce9a53024853f24ac94
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-03-08 21:16:22 +0100

    Add script to create an account for ansible actions

---
+++ lxc-post-create.d/configure-ansible-sudouser.inc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o nounset
+
+ANSIBLEUSER_INC="$(dirname "$0")/settings/ansibleuser.inc.sh"
+if ! [ -f "$ANSIBLEUSER_INC" ]; then
+    echo "ERROR: config file [$ANSIBLEUSER_INC] is required!"
+    return
+fi
+
+source "$ANSIBLEUSER_INC"
+
+chroot "$TGT_ROOT" adduser \
+    --system \
+    --home /var/lib/ansibleuser \
+    --shell /bin/bash \
+    --gecos "Ansible 'sudo' User" \
+    ansibleuser
+
+echo "ansibleuser:$ANSIBLE_USER_PW" | chroot "$TGT_ROOT" chpasswd
+
+echo "ansibleuser ALL = (root) NOPASSWD:ALL" > "$TGT_ROOT"/etc/sudoers.d/ansibleuser
---
+++ settings/ansibleuser.template.inc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# if the variable 'ANSIBLE_USER_PW' is set (and the container template includes
+# the 'configure-ansible-sudouser.sh' script), the account 'ansibleuser' will be
+# created and the value of that variable will be set for that account:
+ANSIBLE_USER_PW="What-A-W0nderfU1-P4ssw0rd"
