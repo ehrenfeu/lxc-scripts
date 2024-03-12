0b60a82bcbddc31dec33425c72f025fc26d08214
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-03-12 21:38:52 +0100

    Install ansibleuser pubkey

--- lxc-post-create.d/configure-ansible-sudouser.inc.sh
+++ lxc-post-create.d/configure-ansible-sudouser.inc.sh
@@ -2,7 +2,10 @@
 
 set -o nounset
 
+ANSIBLEUSER_HOME=/var/lib/ansibleuser
 ANSIBLEUSER_INC="$(dirname "$0")/settings/ansibleuser.inc.sh"
+ANSIBLEUSER_PUBKEY="$(dirname "$0")/settings/ansibleuser_id_ed25519.pub"
+
 if ! [ -f "$ANSIBLEUSER_INC" ]; then
     echo "ERROR: config file [$ANSIBLEUSER_INC] is required!"
     return
@@ -13,9 +16,16 @@ source "$ANSIBLEUSER_INC"
 chroot "$TGT_ROOT" adduser \
     --system \
     --ingroup sudo \
-    --home /var/lib/ansibleuser \
+    --home "$ANSIBLEUSER_HOME" \
     --shell /bin/bash \
     --gecos "Ansible 'sudo' User" \
     ansibleuser
 
+# install the user's public key for ssh-auth:
+mkdir -pv "$TGT_ROOT/$ANSIBLEUSER_HOME"/.ssh
+chmod go-rwx "$TGT_ROOT/$ANSIBLEUSER_HOME"/.ssh
+cp "$ANSIBLEUSER_PUBKEY" "$TGT_ROOT/$ANSIBLEUSER_HOME"/.ssh/authorized_keys
+chroot "$TGT_ROOT" chown -R ansibleuser "$ANSIBLEUSER_HOME"/.ssh
+
+# setting a password is required to use Ansible's "become" mechanism:
 echo "ansibleuser:$ANSIBLE_USER_PW" | chroot "$TGT_ROOT" chpasswd
